431e9f0629a850a0c1c5be6950e29dfd67a53b08
Alex Varughese <alviac2186@gmail.com>
2026-09-06 09:09:40 +1000

    Updated listening ips

--- server/index.ts
+++ server/index.ts
@@ -615,6 +615,6 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(port, () => {
+httpServer.listen(port, "0.0.0.0", () => {
   console.log(`> Socket.io server listening on http://localhost:${port}`);
 });
